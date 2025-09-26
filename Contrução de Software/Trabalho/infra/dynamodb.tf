062571e25422ef9ac63027b199c4ef990a27cbe5
Vitor <vitor.guttler@edu.pucrs.br>
2025-09-26 19:58:40 -0300

    feat terraform

---
+++ Contrução de Software/Trabalho/infra/dynamodb.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d80bb593ee5edbacc6b3bcec4c813e895e897c8dea789540331c2ffb05ce2acf
+size 340
---
+++ Contrução de Software/Trabalho/infra/s3.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8e27c931ada12d8185ed8ecafb3eea0a435df49e5be1f2fb10d022b706c6e8eb
+size 121
